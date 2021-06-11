b33d39b262fd0f53b965fbaf6067412b611a143f
MAwaisMe <muhammad.awais,genius@gmail.com>
2021-06-11 18:37:45 +0500

    team updated

--- components/Team/TeamMembers.js
+++ components/Team/TeamMembers.js
@@ -10,7 +10,7 @@ const TeamMembers = () => {
             <div className="testimonial-card">
               <div className="testimonial-img-wrap">
                 <img
-                  src="assets/images/testimonial.jpg"
+                  src="assets/images/team5.jpg"
                   alt=""
                   className="testimonial-img"
                 />
