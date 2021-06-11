5bfddf2f74eeaba54c4944c0b24a50992cd86652
MAwaisMe <muhammad.awais,genius@gmail.com>
2021-06-11 19:03:52 +0500

    sfergo

--- pages/codeofethics.js
+++ pages/codeofethics.js
@@ -9,7 +9,7 @@ const codeofethics = (props) => {
 
   return (
     <div className="whitepaper-page">
-      <Layout title="White paper" />
+      <Layout title="Code of Ethics" />
       <SubHeader marqueeData={marqueeData} />
       {/* <!---------------------- End Hero Section ---------------------> */}
       {/* <!-- ===================== whitepaper  section  ======================== --> */}
