6a698d5720a8a587ceaafa4b072a7d6f3826f352
MAwaisMe <muhammad.awais,genius@gmail.com>
2021-06-17 12:14:10 +0500

    asad

--- pages/wallet.js
+++ pages/wallet.js
@@ -96,7 +96,7 @@ const Wallet = (props) => {
                   </div>
                   <div className="col-md-6">
                     <div className="about-illustration-wrap">
-                      <img src="assets/images/security.svg" alt="" />
+                      <img src="assets/images/Security-img.png" alt="" />
                       {/* <ReactPlayer /> */}
                     </div>
                   </div>
@@ -228,7 +228,7 @@ const Wallet = (props) => {
           </div>
         </section>
         {/* <!---------------------- Partners Section ---------------------> */}
-      <section className="partners pt-0">
+      <section className="partners">
         <div className="container custom-contain">
           <div className="row text-center">
             <div className="col-md-12">
@@ -316,6 +316,145 @@ const Wallet = (props) => {
             </div>
         </section>
         {/* <!---------------------- End Timer Section ---------------------> */}
+        {/* Second tree */}
+        <div className="timeline-second-tree">
+                <h2 className="timeline-second-tree-heading">YEARN WALLET ROAD MAP</h2>
+                <div className="timeline">
+                  <div className="containers right">
+                    <div className="content timeline-completed">
+                      <h2>01</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/rocket.svg" alt="" />{" "}
+                        <p>Begin working on creation of Wallet and initial layout.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left">
+                    <div className="content timeline-completed">
+                      <h2>02</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/habits.svg" alt="" />{" "}
+                        <p>
+                        Create marketing campaign and YFIC promotional give away for yEARN wallet roll out.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers right">
+                    <div className="content timeline-completed">
+                      <h2>03</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/mobile.svg" alt="" />{" "}
+                        <p>Meeting with Chainlink Labs to discuss API integration and smart contract implementation in the marijuana industry.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left">
+                    <div className="content timeline-completed">
+                      <h2>04</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/listings.svg" alt="" />{" "}
+                        <p>Partner with Roger’s Financial (Marijuana Grower and Cultivator)</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers right">
+                    <div className="content timeline-completed">
+                      <h2>05</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/real-world.svg" alt="" />{" "}
+                        <p>Acquire API key from METRC (seed to sale software) and begin working on completing smart contract integration.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left">
+                    <div className="content timeline-completed">
+                      <h2>06</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/partnership.svg" alt="" />{" "}
+                        <p>Meeting with ABACA banking intermediate to discuss a possible partnership and future use of yEARN Wallet.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers right">
+                    <div className="content">
+                      <h2>07</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/marketing.svg" alt="" />{" "}
+                        <p>July 15th ZED Run Racehorse Giveaway.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left white-check">
+                    <div className="content">
+                      <h2>08</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/bilaxy.svg" alt="" />{" "}
+                        <p>Deliver Beta version of yEARN Wallet for test pilot.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers right white-check">
+                    <div className="content">
+                      <h2>09</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/dollar.svg" alt="" />
+                        <p>Finalize API integration for Chainlink Labs’ smart contracts.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left white-check">
+                    <div className="content">
+                      <h2>10</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/wallet.svg" alt="" />{" "}
+                        <p>Roll out final version of yEARN Wallet with 2500 YFIC giveaway.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers right white-check">
+                    <div className="content">
+                      <h2>11</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/website.svg" alt="" />{" "}
+                        <p>
+                          Apply for gambling license for crypto casino.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left white-check">
+                    <div className="content">
+                      <h2>12</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/wallet.svg" alt="" />{" "}
+                        <p>yEARN Wallet live on Android and IOS App Store.</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers right white-check">
+                    <div className="content">
+                      <h2>13</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/website.svg" alt="" />{" "}
+                        <p>
+                        Implement yEARN Wallet for use of crypto in retail format.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="containers left white-check">
+                    <div className="content">
+                      <h2>14</h2>
+                      <div className="roadmap-image">
+                        <img src="assets/icons/wallet.svg" alt="" />{" "}
+                        <p>RULE THE FUCKING WORLD JK JK HOPEFULLY SOMEONE READS THIS SHIT</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Second tree */}
         {/* <!---------------------- End About Wallet Section ---------------------> */}
       </div>
     </div>
