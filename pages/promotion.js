4e9195528c23cfdce032fc395ccc2b230f353815
Umer Mughal <itsmeumer966@gmail.com>
2021-06-24 20:15:17 +0500

    Add Promotions

--- pages/promotion.js
+++ pages/promotion.js
@@ -9,7 +9,7 @@ const promotion = (props) => {
 
     return (
         <div className="promotion-page">
-            <Layout title="Promotions" favicon="icons/favicon.ico"/>
+            <Layout title="Promotions" favicon="icons/horse.png"/>
             <SubHeader marqueeData={marqueeData} />
             {/* <!---------------------- Promotions Section ---------------------> */}
             <div className="promotions">
